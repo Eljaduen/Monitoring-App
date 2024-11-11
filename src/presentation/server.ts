4790013a3f9869220c7550f528d6e964c6abc8fe
Nelson Eljadue <88507546+Eljaduen@users.noreply.github.com>
2024-11-11 16:45:34 -0500

    primera parte cron

---
+++ src/presentation/server.ts
@@ -0,0 +1,19 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+
+
+export class Server {
+    public static start() {
+        console.log('Server started...');
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                new CheckService(
+                    () => console.log('Success'),
+                    ( error ) => console.log(error)                   
+                ).execute('https://www.google.com/')
+            }
+        );
+    }
+}
